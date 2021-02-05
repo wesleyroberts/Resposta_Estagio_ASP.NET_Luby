c04acfa629e5609d11c565e37bf27f2f427f37e2
wesleyroberts <69482890+wesleyroberts@users.noreply.github.com>
2021-02-04 23:55:47 -0300

    Add files via upload

---
+++ ativvidade 1.1/ativvidade 1.1/Program.cs
@@ -0,0 +1,21 @@
+﻿
+using System;
+
+namespace atividade1
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite um valor!");
+            int valor = int.Parse(Console.ReadLine());
+            Console.WriteLine(calc(valor));
+            Console.ReadKey();
+        }
+        static int calc(int a)
+        {
+            return ((a <= 1) ? 1 : (a * calc(a - 1)));
+        }
+    }
+}
